0b4b660b2394db34d96121ff5fc6dad2cd8dcea5
Lucas Jablon <100323455+LJablon@users.noreply.github.com>
2023-02-18 15:11:42 -0800

    added Flask boiler plate

---
+++ backend/server.py
@@ -0,0 +1,26 @@
+from flask import Flask, jsonify, request
+import qrcode
+
+app = Flask(__name__)
+
+all_books = []
+
+
+@app.route('/')
+def home():
+    return "<h1>go to home/find_event to find a qrcode for an event</h1>"
+
+
+@app.route("/find_event")
+def add():
+    payload = jsonify({
+        "time": "7:30",
+        "name": "soup kitchen",
+        "location": "500 El Camino Real"
+    })
+    return payload
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
