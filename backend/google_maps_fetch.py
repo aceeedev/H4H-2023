441a63aa2c990ffe6734f51e4f13dcbf595ae870
Trashman247 <rnwong@scu.edu>
2023-02-18 15:51:59 -0800

    Add populate

--- backend/google_maps_fetch.py
+++ backend/google_maps_fetch.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
 
-
 load_dotenv()
-print(os.environ.get('GOOGLE_TOKEN'))
+# print(os.environ.get('GOOGLE_TOKEN'))
+
--- backend/server.py
+++ backend/server.py
@@ -4,11 +4,21 @@ import qrcode
 app = Flask(__name__)
 
 all_books = []
-
+# [homeless-shelter, soup-kitchen, food-drive, homeless-service, homeless-hospital]
 
 @app.route('/')
 def home():
     return "<h1>go to home/find_event to find a qrcode for an event</h1>"
+
+@app.route('/populate')
+def populate():
+    lat = request.args.get('lat', default = 0, type = float)
+    long =  request.args.get('long', default = 0, type = float)
+
+
+
+
+
 
 
 @app.route("/find_event")
