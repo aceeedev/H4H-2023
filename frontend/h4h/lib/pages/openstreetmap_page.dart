45e3bfcc4025f9d02c087f84ee5c48c77d297e73
iorais <101996873+iorais@users.noreply.github.com>
2023-02-18 16:22:03 -0800

    map stuff

--- frontend/h4h/lib/pages/openstreetmap_page.dart
+++ frontend/h4h/lib/pages/openstreetmap_page.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-
 import 'dart:async';
 
 class MapSample extends StatefulWidget {
@@ -22,14 +21,13 @@ class MapSampleState extends State<MapSample> {
   static const CameraPosition _kLake = CameraPosition(
       bearing: 192.8334901395799,
       target: LatLng(37.43296265331129, -122.08832357078792),
-      tilt: 59.440717697143555,
       zoom: 19.151926040649414);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: GoogleMap(
-        mapType: MapType.hybrid,
+        mapType: MapType.normal,
         initialCameraPosition: _kGooglePlex,
         onMapCreated: (GoogleMapController controller) {
           _controller.complete(controller);
