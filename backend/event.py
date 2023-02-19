3cf4f5b0fd3c621b25d8f71971290e76a583948c
Lucas Jablon <100323455+LJablon@users.noreply.github.com>
2023-02-18 20:00:19 -0800

    added find address method

--- backend/event.py
+++ backend/event.py
@@ -1,8 +1,21 @@
 class EventManager:
-    def create_event(self, time: str, name: str, location: list[float], description: str):
+    def create_event(self, time: str, name: str, cords: list[float], location: str, description=""):
         return ({
             "time": time,
             "name": name,
+            "cords": cords,
             "location": location,
             "description": description
         })
+    
+    def find_address(self, address_components: list[dict]):
+
+        address = ""
+        for component in address_components:
+            address += component["long_name"]
+            if "locality" or "political" in component["types"]: 
+                break
+        print(address)
+        return address 
+            
+
